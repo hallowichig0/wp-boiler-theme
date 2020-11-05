5ddc3195f54df47e62cd5a9b80bb4cae13147ee8
Jayson Garcia <53173775+hallowichig0@users.noreply.github.com>
2020-11-05 13:09:37 +0800

    Update theme_filter.php

--- inc/theme_filter.php
+++ inc/theme_filter.php
@@ -1,9 +1,14 @@
 <?php
 
 /**
- * Remove automatically generated <p> & </br> tag in content
+ * Remove automatically </br> tag in content
  */
+function better_wpautop($p){
+    return wpautop($p,$br = false);
+}
 remove_filter( 'the_content', 'wpautop' );
+add_filter( 'the_content', 'better_wpautop' , 99);
+add_filter( 'the_content', 'shortcode_unautop',100 );
 
 /**
  * Remove automatically generated <p> & </br> tag in comment
