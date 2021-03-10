116a8d1401c28f127b52939a9bd327acb0dee43c
Jayson Garcia <53173775+hallowichig0@users.noreply.github.com>
2021-03-10 10:30:18 +0800

    Update content-none.php

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -12,7 +12,21 @@
 <section class="no-results not-found">
 	<!-- .page-content -->
 	<div class="page-content">
+	<?php
+	if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+
 		<div class="alert alert-info">No content assiged yet.</div>
+
+	<?php elseif ( is_search() ) : ?>
+		<div class="alert alert-info">Sorry, but nothing matched your search terms. Please try again with some different keywords.</div>
+		<?php get_search_form(); ?>
+
+	<?php else : ?>
+
+		<div class="alert alert-info">It seems we can't find what you're looking for. Perhaps searching can help.</div>
+		<?php get_search_form(); ?>
+
+	<?php endif; ?>
 	</div>
 </section>
 <!-- .no-results -->
