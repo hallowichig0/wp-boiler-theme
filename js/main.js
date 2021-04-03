80624faf6ab2f4c652c7571771ad8e65a0782195
Jayson Garcia <53173775+hallowichig0@users.noreply.github.com>
2021-04-03 17:23:31 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -78,7 +78,19 @@
     * Document Ready
     */
     $doc.ready(function(){
-        
+        // CF7 validation events for changing response CSS classes
+        // document.addEventListener( 'wpcf7invalid', function( event ) {
+        //   $('.wpcf7-response-output').addClass('alert alert-danger');
+        // }, false );
+        // document.addEventListener( 'wpcf7spam', function( event ) {
+        //   $('.wpcf7-response-output').addClass('alert alert-warning');
+        // }, false );
+        // document.addEventListener( 'wpcf7mailfailed', function( event ) {
+        //   $('.wpcf7-response-output').addClass('alert alert-warning');
+        // }, false );
+        // document.addEventListener( 'wpcf7mailsent', function( event ) {
+        //   $('.wpcf7-response-output').addClass('alert alert-success');
+        // }, false );
     });
 
 })( jQuery );
