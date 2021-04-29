56651e9775b41da7e519f85e935f4c7fd7dfeb63
Jayson Garcia <jaysongarcia@Jaysons-MacBook-Pro.local>
2021-04-29 22:20:37 +0800

    Fixed error in header.php. Update header.php

--- footer.php
+++ footer.php
@@ -14,27 +14,28 @@ $footer_bg = get_theme_mod('footer_background_setting');
 $footer_copyrightColor = get_theme_mod('footer_copyrightColor_setting');
 $footer_copyright = get_theme_mod('footer_copyrightText_setting');
 ?>
-</main><!-- #primary -->
+    </main><!-- #primary -->
 
-<footer id="footer" class="site-footer py-4" style="background-color:<?php echo !empty($footer_bg) ? $footer_bg : '#343a40'; ?>">
-    <div class="container">
-        <div class="row">
-            <p class="m-0" style="color:<?php echo !empty($footer_copyrightColor) ? $footer_copyrightColor : '#ffffff'; ?>">
-                <?php if(isset($footer_copyright) && !empty($footer_copyright)): ?>
-                    Copyright &copy; <?php echo $footer_copyright; ?>
-                <?php else: ?>
-                    Copyright &copy; Jayson Garcia 2019-<?php echo date("Y"); ?>
-                <?php endif; ?>
-            </p>
+    <footer id="footer" class="site-footer py-4" style="background-color:<?php echo !empty($footer_bg) ? $footer_bg : '#343a40'; ?>">
+        <div class="container">
+            <div class="row">
+                <p class="m-0" style="color:<?php echo !empty($footer_copyrightColor) ? $footer_copyrightColor : '#ffffff'; ?>">
+                    <?php if(isset($footer_copyright) && !empty($footer_copyright)): ?>
+                        Copyright &copy; <?php echo $footer_copyright; ?>
+                    <?php else: ?>
+                        Copyright &copy; Jayson Garcia 2019-<?php echo date("Y"); ?>
+                    <?php endif; ?>
+                </p>
+            </div>
         </div>
-    </div>
-    <!-- Back to top -->
-    <div id="back2top" class="backtotop">
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="120px" version="1.1" height="120px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" class="">
-            <path fill="#FFFFFF" d="M3.352,48.296l28.56-28.328l28.58,28.347c0.397,0.394,0.917,0.59,1.436,0.59c0.52,0,1.04-0.196,1.436-0.59   c0.793-0.787,0.793-2.062,0-2.849l-29.98-29.735c-0.2-0.2-0.494-0.375-0.757-0.475c-0.75-0.282-1.597-0.107-2.166,0.456   L0.479,45.447c-0.793,0.787-0.793,2.062,0,2.849C1.273,49.082,2.558,49.082,3.352,48.296z" data-original="#1D1D1B" class="active-path" data-old_color="#FDFDFB"/>
-        </svg>
-    </div>
-</footer>
+        <!-- Back to top -->
+        <div id="back2top" class="backtotop">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="120px" version="1.1" height="120px" viewBox="0 0 64 64" enable-background="new 0 0 64 64" class="">
+                <path fill="#FFFFFF" d="M3.352,48.296l28.56-28.328l28.58,28.347c0.397,0.394,0.917,0.59,1.436,0.59c0.52,0,1.04-0.196,1.436-0.59   c0.793-0.787,0.793-2.062,0-2.849l-29.98-29.735c-0.2-0.2-0.494-0.375-0.757-0.475c-0.75-0.282-1.597-0.107-2.166,0.456   L0.479,45.447c-0.793,0.787-0.793,2.062,0,2.849C1.273,49.082,2.558,49.082,3.352,48.296z" data-original="#1D1D1B" class="active-path" data-old_color="#FDFDFB"/>
+            </svg>
+        </div>
+    </footer>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>
--- header.php
+++ header.php
@@ -14,7 +14,7 @@ $get_template_directory = get_template_directory();
 $custom_logo_id = get_theme_mod( 'custom_logo' );
 $header_logo_src = wp_get_attachment_image_url( $custom_logo_id , 'full' );
 $header_logo_srcset = wp_get_attachment_image_srcset( $custom_logo_id , 'full' );
-$header_logo_sizes = wp_get_attachment_image_sizes( $custom_logo_id , 'full' 
+$header_logo_sizes = wp_get_attachment_image_sizes( $custom_logo_id , 'full' );
 
 // Kirki Variables
 $header_bg = get_theme_mod('header_background_field_setting');
@@ -37,112 +37,113 @@ $header_bg = get_theme_mod('header_background_field_setting');
 </head>
 <body <?php body_class(); ?>>
 	<?php wp_body_open(); ?>
-	
-	<!-- Loading Animation -->
-	<div class="pre-loader">
-		<div class="loader"></div>
-		<p>Please wait while loading...</p>
-	</div>
 
-	<!-- Navigation -->
-	<header id="header" class="nav-header">
-		<nav class="navbar nav-top" style="background-color: <?php echo !empty($header_bg) ? $header_bg : '#ffffff'; ?>">
-			<div class="container px-sm-0">
-				<div class="navbar-nav">
-					<?php if(has_custom_logo()): ?>
-						<a href="<?php echo esc_url( home_url( '/' )); ?>">
-							<img src="<?php echo $header_logo_src; ?>" rcset="<?php echo $header_logo_srcset; ?>" sizes="<?php echo $header_logo_sizes; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" height="40px" width="100px">
-						</a>
-					<?php
-					else:
-					$header_logoTitleColor = get_theme_mod('header_logoColorTitle_field_setting');
-					$header_logoSubtitleColor = get_theme_mod('header_logoColorSubTitle_field_setting');
-					?>
-						<a class="navbar-brand col-md-6 px-0" style="color:<?php echo !empty($header_logoTitleColor) ? $header_logoTitleColor : '#000000'; ?>" class="navbar-brand" href="<?php echo esc_url( home_url( '/' )); ?>">
-							<h3 class="font-weight-bold" style="margin-bottom:0.3rem;"><?php esc_url(bloginfo('name')); ?></h3>
-							<?php if(get_bloginfo('description')): ?>
-								<h6 class="mb-0" style="color:<?php echo !empty($header_logoSubtitleColor) ? $header_logoSubtitleColor : '#000000'; ?>">
-									<?php echo get_bloginfo('description'); ?>
-								</h6>
-							<?php endif; ?>
-						</a>
-					<?php endif; ?>
-				</div>
+	<div id="page-wrapper" class="page-wrapper">
+		<!-- Loading Animation -->
+		<div class="pre-loader">
+			<div class="loader"></div>
+			<p>Please wait while loading...</p>
+		</div>
 
-				<div class="navbar-nav ml-sm-auto social-media">
-					<?php
-					// header contact
-					$header_contactText = get_theme_mod('header_contactText_field_setting');
-					$header_contactTextColor = get_theme_mod('header_contactColor_field_setting');
-					if(!empty($header_contactText) && isset($header_contactText)):
-					?>
-						<div class="navbar-brand col-md-6 px-0" style="padding-top:10px">
-							<a style="color:<?php echo !empty($header_contactTextColor) ? $header_contactTextColor : '#000000'; ?>" href="tel:<?php echo $header_contactText; ?>">
-							<h5 class="mb-0">
-								<i class="fa fa-phone" aria-hidden="true" style="font-weight: 900"></i>
-								<?php echo $header_contactText; ?>
-							</h5>
+		<!-- Navigation -->
+		<header id="header" class="nav-header">
+			<nav class="navbar nav-top" style="background-color: <?php echo !empty($header_bg) ? $header_bg : '#ffffff'; ?>">
+				<div class="container px-sm-0">
+					<div class="navbar-nav">
+						<?php if(has_custom_logo()): ?>
+							<a href="<?php echo esc_url( home_url( '/' )); ?>">
+								<img src="<?php echo $header_logo_src; ?>" rcset="<?php echo $header_logo_srcset; ?>" sizes="<?php echo $header_logo_sizes; ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" height="40px" width="100px">
 							</a>
-						</div>
-					<?php endif; ?>
-					<?php
-					// social media
-					$media_repeater = get_theme_mod('header_social_media_repeater');
-					if(!empty($media_repeater) && isset($media_repeater)):
-					?>
-						<div class="navbar-brand col-md-6 px-0 py-0 social-menu">
-							<?php
-							$social_classes = '';
-							foreach($media_repeater as $social_media):
-								$select_media = $social_media['header_social_media_select'];
-								$link_media = $social_media['header_social_media_link'];
-								if($select_media == 'fb'){
-									$social_classes = 'fa-facebook social-icon-fb';
-								}
-								elseif($select_media == 'instagram'){
-									$social_classes = 'fa-instagram social-icon-instagram';
-								}
-								elseif($select_media == 'twitter'){
-									$social_classes = 'fa-twitter social-icon-twitter';
-								}
-								elseif($select_media == 'pinterest'){
-									$social_classes = 'fa-pinterest social-icon-pinterest';
-								}
-								elseif($select_media == 'linkedin'){
-									$social_classes = 'fa-linkedin-square social-icon-linkedin';
-								}
-							?>
-								<a href="<?php echo $link_media; ?>" target="_blank"> <i class="social-icon fa <?php echo $social_classes; ?>" aria-hidden="true"></i></a>
-							<?php endforeach; ?>
-						</div>
-					<?php endif; ?>
+						<?php
+						else:
+						$header_logoTitleColor = get_theme_mod('header_logoColorTitle_field_setting');
+						$header_logoSubtitleColor = get_theme_mod('header_logoColorSubTitle_field_setting');
+						?>
+							<a class="navbar-brand col-md-6 px-0" style="color:<?php echo !empty($header_logoTitleColor) ? $header_logoTitleColor : '#000000'; ?>" class="navbar-brand" href="<?php echo esc_url( home_url( '/' )); ?>">
+								<h3 class="font-weight-bold" style="margin-bottom:0.3rem;"><?php esc_url(bloginfo('name')); ?></h3>
+								<?php if(get_bloginfo('description')): ?>
+									<h6 class="mb-0" style="color:<?php echo !empty($header_logoSubtitleColor) ? $header_logoSubtitleColor : '#000000'; ?>">
+										<?php echo get_bloginfo('description'); ?>
+									</h6>
+								<?php endif; ?>
+							</a>
+						<?php endif; ?>
+					</div>
+
+					<div class="navbar-nav ml-sm-auto social-media">
+						<?php
+						// header contact
+						$header_contactText = get_theme_mod('header_contactText_field_setting');
+						$header_contactTextColor = get_theme_mod('header_contactColor_field_setting');
+						if(!empty($header_contactText) && isset($header_contactText)):
+						?>
+							<div class="navbar-brand col-md-6 px-0" style="padding-top:10px">
+								<a style="color:<?php echo !empty($header_contactTextColor) ? $header_contactTextColor : '#000000'; ?>" href="tel:<?php echo $header_contactText; ?>">
+								<h5 class="mb-0">
+									<i class="fa fa-phone" aria-hidden="true" style="font-weight: 900"></i>
+									<?php echo $header_contactText; ?>
+								</h5>
+								</a>
+							</div>
+						<?php endif; ?>
+						<?php
+						// social media
+						$media_repeater = get_theme_mod('header_social_media_repeater');
+						if(!empty($media_repeater) && isset($media_repeater)):
+						?>
+							<div class="navbar-brand col-md-6 px-0 py-0 social-menu">
+								<?php
+								$social_classes = '';
+								foreach($media_repeater as $social_media):
+									$select_media = $social_media['header_social_media_select'];
+									$link_media = $social_media['header_social_media_link'];
+									if($select_media == 'fb'){
+										$social_classes = 'fa-facebook social-icon-fb';
+									}
+									elseif($select_media == 'instagram'){
+										$social_classes = 'fa-instagram social-icon-instagram';
+									}
+									elseif($select_media == 'twitter'){
+										$social_classes = 'fa-twitter social-icon-twitter';
+									}
+									elseif($select_media == 'pinterest'){
+										$social_classes = 'fa-pinterest social-icon-pinterest';
+									}
+									elseif($select_media == 'linkedin'){
+										$social_classes = 'fa-linkedin-square social-icon-linkedin';
+									}
+								?>
+									<a href="<?php echo $link_media; ?>" target="_blank"> <i class="social-icon fa <?php echo $social_classes; ?>" aria-hidden="true"></i></a>
+								<?php endforeach; ?>
+							</div>
+						<?php endif; ?>
+					</div>
 				</div>
-			</div>
-		</nav>
+			</nav>
 
-		<nav class="navbar navbar-expand-md nav-bottom" style="background-color: #343a40">
-			<div class="container">
-				<button class="navbar-toggler collapsed navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="icon-bar top-bar"></span>
-					<span class="icon-bar middle-bar"></span>
-					<span class="icon-bar bottom-bar"></span>
-				</button>
-				<?php
-				wp_nav_menu( array(
-					'theme_location'    => 'primary',
-					'depth'             => 2, // 1 = no dropdowns, 2 = with dropdowns.
-					'container'         => 'div',
-					'container_class'   => 'navbar-collapse collapse',
-					'container_id'      => 'navbarToggler',
-					'menu_class'        => 'nav navbar-nav mx-auto mt-3 mt-md-0',
-					'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
-					'walker'            => new WP_Bootstrap_Navwalker(),
-					'add_li_class'  => '', // set classes in li tag
-					'add_a_class'  => 'nav-link px-md-4 ml-2 ml-md-0', // set classes in a tag
-				) );
-				?>
-			</div>
-		</nav>
+			<nav class="navbar navbar-expand-md nav-bottom" style="background-color: #343a40">
+				<div class="container">
+					<button class="navbar-toggler collapsed navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar top-bar"></span>
+						<span class="icon-bar middle-bar"></span>
+						<span class="icon-bar bottom-bar"></span>
+					</button>
+					<?php
+					wp_nav_menu( array(
+						'theme_location'    => 'primary',
+						'depth'             => 2, // 1 = no dropdowns, 2 = with dropdowns.
+						'container'         => 'div',
+						'container_class'   => 'navbar-collapse collapse',
+						'container_id'      => 'navbarToggler',
+						'menu_class'        => 'nav navbar-nav mx-auto mt-3 mt-md-0',
+						'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+						'walker'            => new WP_Bootstrap_Navwalker(),
+						'add_li_class'  => '', // set classes in li tag
+						'add_a_class'  => 'nav-link px-md-4 ml-2 ml-md-0', // set classes in a tag
+					) );
+					?>
+				</div>
+			</nav>
 
-	</header>
-	<main id="primary" class="content-area main-wrapper">
+		</header>
+		<main id="primary" class="content-area main-wrapper">
